780b1b3a17a1790b44111f3e2cdd05c6bd67201c
Your Name <vikas.ramtel@portpro.io>
2026-06-18 23:02:39 +0545

    feat: implement job update and delete functionality in tracker controller

--- src/server/controller/tracker.js
+++ src/server/controller/tracker.js
@@ -31,3 +31,64 @@ export const addJobs = async (req, res) => {
         res.status(500).json({ message: "Server Error", error: error.message});
     }
 };
+
+export const updateJobApp = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { companyName, notes, status, jobType, jobTitle, appliedDate } = req.body;
+
+        const updatedData = {};
+        if(companyName !== undefined){
+            updatedData.companyName = companyName;
+        }
+        if(notes !== undefined){
+            updatedData.notes = notes;
+        }
+        if(status !== undefined){
+            updatedData.status = status;
+        }
+        if(jobType !== undefined){
+            updatedData.jobType = jobType;
+        }
+        if(jobTitle !== undefined){
+            updatedData.jobTitle = jobTitle;
+        }
+        if(appliedDate !== undefined){
+            updatedData.appliedDate = appliedDate;
+        }
+
+        const updateJob = await application.update( updatedData, {
+            where: { id }
+        });
+
+        res.status(200).json({
+            message: "Job updated successfully",
+            updateJob,
+        });
+
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+
+
+    }
+}
+
+
+export const deleteJob = async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const deleteJob = await application.destroy({
+            where: { id }
+        });
+
+        res.status(200).json({
+            message: "Job deleted successfully",
+            deleteJob,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+
+}
--- src/server/routes/route.js
+++ src/server/routes/route.js
@@ -1,8 +1,12 @@
 import {Router} from "express";
-import {addJobs, getAllJobs} from "../controller/tracker.js";
+import {addJobs, getAllJobs, updateJobApp, deleteJob} from "../controller/tracker.js";
 const app = Router();
 
 app.get("/getJobs", getAllJobs);
 app.post("/addjob", addJobs);
+app.put("/updatedJob/:id", updateJobApp);
+app.delete("/deleteJob/:id", deleteJob);
+
+
 
 export default app; 
